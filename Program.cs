0247e65824d289d64d649ede82d6bcbc2aea9c77
manishjangra1 <dev.jangramanish@gmail.com>
2024-09-17 12:05:49 +0530

    programStructure

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//C# program Structure
+
+using System;
+//System class is included because it has many functions and we want to run program on system
+
+namespace Programs{
+// namespace is collection of multiple classes
+    public class Program{
+// Class to contain object and methods
+        public static void Main(string[] args){
+// Main method is starting point of the program
+
+            Console.WriteLine("Hello, Manish");
+// The above syntax will execute code on the Console
+
+        }
+    }
+}
